256fe0700fa34b8e63041b7a97898ac9032e0566
MiguelAponte18 <109483620+MiguelAponte18@users.noreply.github.com>
2024-11-30 23:45:00 -0400

    primer

---
+++ formulario_php.js
@@ -0,0 +1,110 @@
+
+ function formularioT(){
+const d= document;
+const $inputs = d.querySelectorAll(".formulario [required]")
+const $form = d.querySelector(".formulario")
+// const $iAsunto = d.querySelector("#asunto")
+// const $Email = d.querySelector("#email")
+// const $Campo = d.querySelector("#campo"), 
+const $enviar = d.querySelector(".enviar");
+
+const objeto ={
+    nombre:"",
+    asunto:"",
+    email:"",
+    campo:""
+}
+
+$inputs.forEach(e=>{
+    const $span = d.createElement("span");
+    $span.id = e.name;
+    $span.textContent = e.title;
+    $span.classList.add("contact-form-error","none");
+    e.insertAdjacentElement("afterend",$span)
+})
+
+const escribir=(n)=>{
+
+   
+  //  nuevo.textContent = n.id;
+  
+       
+
+}
+
+let numeros = /^[0-9]$/g
+console.log($enviar)
+    
+    d.addEventListener("input",e=>{
+          if(e.target.matches(".formulario [required]")){
+            let $input = e.target,
+            pattern = $input.pattern || $input.dataset.pattern;
+            // console.log($input,pattern)
+
+            if(pattern && $input.value !== ""){
+                let expres = new RegExp(pattern);
+               return  !expres.exec($input.value)? d.querySelector(`#${$input.name}`).classList.add("is-active"): d.querySelector(`#${$input.name}`).classList.remove("is-active");
+
+                // console.log("tiene patron")
+            }
+
+            if(!pattern){
+                console.log("NO tiene patron")
+                return  $input.value=== ""? d.querySelector(`#${$input.name}`).classList.add("is-active"): d.querySelector(`#${$input.name}`).classList.remove("is-active");
+
+            }
+
+
+          }
+
+    }) ;
+   //para el envio del correo se necesita un servidor que ppermita la codificacion para emails, por lo tanto el servidor local desde apache nose puede, se necesita un programa especial
+
+   
+    d.addEventListener("submit",e=>{
+     e.preventDefault()
+     alert("eviando formulario");
+     const $loader = d.querySelector(".contact-form-loader"),
+     $enviados = d.querySelector(".contact-response");
+     const options = {
+        method: "POST",
+        body: new FormData(e.target),
+        mode: "cors"//esto se aciva para que permita el intercambio de informacion entre servidores, corss son reglas o politias que indican que los recursos que consuma ajax tienen que esstar en el mismo servidor, por defecto es no-cors
+    }
+     fetch("assets/send_mail.php",options)
+     .then(res=>res.ok?res.json():Promise.reject(res))
+     .then(res=>{
+        console.log(res);
+        
+        $loader.classList.add("none");
+        $enviados.innerHTML =`<p>${res.message}</p>`;
+        $enviados.classList.remove("none")
+        $form.reset(); //para resetear un formulario
+       
+     })
+     .catch(err=>{
+        let message = err.statusText| "Ocurrio un error";
+        $enviados.innerHTML = `<p>Error: ${err.status}: ${message}</p>`
+        console.log(err)
+     })
+     .finally(()=>{
+        setTimeout(() => $enviados.classList.add("none"), 3000);
+        $enviados.innerHTML = "";
+     })
+    
+     $loader.classList.remove("none")
+
+
+    //  setTimeout(() => {
+       
+    //     setTimeout(() => $enviados.classList.add("none"), 3000);
+    //  }, 3000);
+
+    
+    })
+
+
+}
+
+formularioT();
+    
